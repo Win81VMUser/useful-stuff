a0459822bb546a783bdb4804f77f0ba5945fc0e4
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-03-28 14:49:48 +0200

    Create ChromeRDPforGShell.sh

---
+++ Linux/ChromeRDPforGShell.sh
@@ -0,0 +1,10 @@
+echo "Installer will install necessary files for cloud VPS"
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome-remote-desktop_current_amd64.deb 
+sudo dpkg --configure -a
+sudo apt --fix-broken install -y
+echo "Go to https://remotedesktop.google.com/headless and click on debian/linux and copy it here"
+echo "Now installer will install XFCE4 desktop... Please wait...
+sudo apt install xfce4 -y
+echo "Go to https://remotedesktop.google.com/access and look for your desktop! HAVE FUN!!!
+echo "BYE!!!"
