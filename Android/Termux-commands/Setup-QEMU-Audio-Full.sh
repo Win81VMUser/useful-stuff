7c7ca6ed85922fb50319ea26a558c12c9cf05b56
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-03-29 19:02:53 +0200

    Create Setup-QEMU-Audio-Full.sh

---
+++ Android/Termux-commands/Setup-QEMU-Audio-Full.sh
@@ -0,0 +1,16 @@
+echo "Setup will now install and configure audio files..."
+pkg update
+pkg install wget && wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh && chmod +x setup-audio.sh && ./setup-audio.sh
+echo "Now setup will run some scripts on pulseaudio..."
+pkg install pulseaudio
+pulseaudio --start
+pulseaudio -D
+pulseaudio -v
+echo "Now setup will install x11-repo and qemu-system-x86_64 WITH SDL..."
+pkg update
+pkg install x11-repo
+pkg install qemu-system-x86_64
+echo "Now setup will download a hdd file(if Win8.1VMUser uploaded it into disk images)
+echo "No disk found here... Wait... We can test it in another way!"
+echo "Now qemu is starting... To quit type "quit" or press ctrl+c"
+qemu-system-x86_64 -boot n -monitor stdio -accel tcg -m 256 -vnc :1
