c490c4fabd739b664223192030d6a7f1847a8bdf
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-04-03 21:38:42 +0200

    Update ChromeRDPforGShell.sh

--- Linux/ChromeRDPforGShell.sh
+++ Linux/ChromeRDPforGShell.sh
@@ -4,8 +4,9 @@ sudo dpkg -i chrome-remote-desktop_current_amd64.deb
 sudo dpkg --configure -a
 sudo apt --fix-broken install -y
 echo "Go to https://remotedesktop.google.com/headless and click on debian/linux and copy it here"
-echo "Now installer will install XFCE4 desktop... Please wait..."
-sudo apt install xfce4 -y
+echo "   "
+echo "   "
+echo "   "
 echo "Go to https://remotedesktop.google.com/headless and click on debian/linux and copy it here"
 echo "Now go to https://remotedesktop.google.com/access and look for your desktop! HAVE FUN!!!
 echo "BYE!!!"
