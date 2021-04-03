915c0af2f325f17fef888a0b90caaee71f402f78
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-04-03 21:37:45 +0200

    Create OBS-Installer.sh

---
+++ Linux/OBS-Installer.sh
@@ -0,0 +1,11 @@
+echo "Setup will install files and OBS Studio!"
+sudo apt install ffmpeg -y
+clear
+echo "Press ENTER now!"
+sudo add-apt-repository ppa:obsproject/obs-studio
+clear
+echo "Now installing OBS Studio"
+sudo "apt install obs-studio -y
+clear
+echo "Everything is done!"
+echo "Bye!!!"
