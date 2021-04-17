b994d10ad2ad73e30e33860db5201e2b0c177a39
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-04-17 21:00:23 +0200

    Update ChromeRDPForLinux.sh

--- Android/Termux-commands/ChromeRDPForLinux.sh
+++ Android/Termux-commands/ChromeRDPForLinux.sh
@@ -1,8 +1,8 @@
 echo "Installer will install necessary files for cloud VPS"
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome-remote-desktop_current_amd64.deb 
-sudo dpkg --configure -a
-sudo apt --fix-broken install -y
+dpkg -i chrome-remote-desktop_current_amd64.deb 
+dpkg --configure -a
+apt --fix-broken install -y
 echo "Go to https://remotedesktop.google.com/headless and click on debian/linux and copy it here"
 echo "   "
 echo "   "
