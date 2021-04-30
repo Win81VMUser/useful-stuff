093a1434a9ac3066c16c8a1bf5b9365e9564b682
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-04-30 19:38:20 +0200

    Create MakeXFCE4Desktop.sh

---
+++ MakeXFCE4Desktop.sh
@@ -0,0 +1,23 @@
+echo "Starting update..."
+apt update
+echo "Selecting BEST desktop for you..."
+echo "Please Wait..."
+apt install xfce4 apt install xfce4-whiskermenu-plugin -y
+clear
+echo "Done! Start xsdl and run these commands..."
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+echo "Starting XFCE4..."
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+export DISPLAY=127.0.0.1:0 PULSE_SERVER=tcp:127.0.0.1:4713
+export DISPLAY=127.0.0.1:0 PULSE_SERVER=tcp:127.0.0.1:4713
+export DISPLAY=127.0.0.1:0 PULSE_SERVER=tcp:127.0.0.1:4713
+echo "Starting SERVER"
+echo "DONE! HAVE FUN!!!"
+startxfce4
